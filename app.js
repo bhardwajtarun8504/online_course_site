6b5bc3a1b062a1833b6ce6c86779de5081a5e7b6
bhardwajtarun8504 <tarunbhardwaj8504@gmail.com>
2021-09-07 23:55:45 +0530

    recent commit

--- app.js
+++ app.js
@@ -2,34 +2,21 @@
 const { name } = require("ejs");
 var express = require("express"),
     mongoose = require("mongoose"),
-    passport = require("passport"),
     bodyParser = require("body-parser"),
     User = require("./models/User");
-// LocalStrategy         = require("passport-local"),
-// passportLocalMongoose = require("passport-local-mongoose");
 const port = 8000;
 
-mongoose.connect("mongodb://localhost/login_users", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect("mongodb://localhost/login_users", { useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex: true});
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(require("express-session")({
-//     secret: "Rusty is the best and cutest dog in the world",
-//     resave: false,
-//     saveUninitialized: false
-// }));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
 
-// passport.use(new LocalStrategy(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser())
+//============S
 
-//============
 // ROUTES
 //============
 
@@ -44,77 +31,68 @@ app.get("/register", function (req, res) {
     res.render("register",{text:  ""})
 });
 //handling user sign up
-app.post("/register", async (req, res)=>{
-//         User.register(new User({email : req.body.email,username: req.body.username}), req.body.password, function(err, user){
-//     if(err){
-//         res.send(err);
-//         return res.render('register');
-//     }
-//     passport.authenticate("local")(req, res, function(){
-//           res.redirect("/secret");
-//     });
-// }); 
+app.post("/register",async (req, res)=>{
 try {
     const userregister = new User({
         username : req.body.username,
         email : req.body.email, 
         password : req.body.password,
         contact : req.body.contact
-    }) 
-    const registered = await userregister.save();
-    res.render("secret",{text: req.body.username});
+    })
+    const registered =await userregister.save();
+    res.render("login",{text: "Successsfully Registerd !! Login Now"});
 } catch (error) {
     res.render("register",{text:  "already registerd with email or username"})
 }
 });
 
-// LOGIN ROUTES
-// app.get("/secret", function(req, res){
-//     res.render("secret",{text: req.user.username}); 
-//  });
 //render login form
 app.get("/login", function (req, res) {
     res.render("login",{text:"   "});
 });
 
-// login logic
-// middleware
-// app.post("/login", passport.authenticate("local", {
-//     successRedirect: "/secret",
-//     failureRedirect: "/login",
-// }) ,function(req, res){
-// });
-
 app.post("/login", async (req, res) => {
-    // console.log(req.user.username);
     try {
         const email = req.body.email;
         const password = req.body.password;
-        const useremail = await User.findOne({ email: email })
+        const useremail =await User.findOne({ email : email })
+        // console.log(useremail.email);
         if (useremail.password === password) {
-            res.render("secret", { text: useremail.username , id: useremail.id});
+            res.render("secret", {text: useremail.username ,id: useremail.id});
+            // console.log("password compared");
         }
         else {
-            res.render("login",{text: "Password not Matched"})
+            res.render("login",{text: "Password not Matched"});
         }
     } catch (error) {
         res.render("login",{text: "Email Not registerd Yet"})
     }
 })
 
+app.get("/forget_password",(req,res)=>{
+    res.render("forget_password",{text: "  "});
+});
+
+app.post("/forget_password",async (req,res) =>{
+    try {
+    const newpassword = req.body.new_password;
+    const repassword = req.body.confirm_password;
+    if(newpassword === repassword){
+        await User.updateOne({ email: req.body.email},{$set: {password : newpassword}})
+        res.render("login",{text: "Password Update"})
+    }
+    else{
+        res.render("forget_Password",{text: "confirm password not matched"})
+        }
+    } catch (error) {
+        res.render("forget_Password",{text: "This Email is Not Registerd Yet"})
+    }    
+})
+
 app.get("/logout", function (req, res) {
-    req.logout();
     res.redirect("/");
 });
 
-
-// function isLoggedIn(req, res, next){
-//     if(req.isAuthenticated()){
-//         console.log('User logged in successfully');
-//         return next();
-//     }
-//     res.redirect("/login");
-// }
 
 // // course registering 
 app.get('/coding_course', (req, res) => {
@@ -140,23 +118,22 @@ app.get('/register-course', (req, res) => {
     res.render('courses/course_register');
 })
 
-app.get('/setting', (req, res) => {
-    res.render("courses/setting")
+
+// user profile 
+app.get('/user_profile/:id', async (req, res) => {
+    const user_data = await User.findById(req.params.id);
+    // console.log(user_data);
+    // console.log(req.user);
+    res.render('courses/profile', {user_data: user_data});
 })
 
-// app.post("/setting",async(req,res)=>{
-// //  const updateuser =await User.updateOne({email: req.user.email, password:req.body.password},{$set: {contact:req.body.contact , status:req.body.status}});
-// })
+// user account details
+app.get('/user_account/:id',async (req, res) => {
+    const user_data = await User.findById(req.params.id);
+    res.render('courses/account', {user_data: user_data , text:" "});
+    // res.render('courses/account',{text:" "});
+});
 
-app.get('/user_profile/', async (req, res) => {
-    const user_data = await User.find({});
-    console.log(req.user);
-    res.render('courses/profile');
-})
-
-app.get('/user_account', (req, res) => {
-    res.render('courses/account',{text:""});
-})
 app.post("/user_account", async (req, res) => {
     try {
         const useremail = await User.findOne({ email: req.body.email })
@@ -165,24 +142,44 @@ app.post("/user_account", async (req, res) => {
             res.status(201).send("profile update successfully")
         }
         else {
-        res.render('courses/account',{text:"Email or password not matched"});
+        res.render('courses/account',{user_data: user_data, text:"Email or password not matched"});
         }
-        
     } catch (error) {
-        res.render('courses/account',{text:"This Email is not Registered"});
+        res.render('courses/account',{user_data: user_data, text:"This Email is not Registered"});
     }
-  
 });
 
+app.get('/setting', (req, res) => {
+    res.render("courses/setting",{text:" "});
+})
+
 app.post("/password_update", async (req, res) => {
-  console.log(user_details.username)
+    try {
+        const newpassword = req.body.new_password;
+        const repassword = req.body.retype_password;
+        if(newpassword === repassword){
+            const useremail = await User.findOne({ email: req.body.email })
+            if(useremail.password === req.body.current_password){
+            const updatepassword = await User.updateOne({ email: req.body.email},{$set: {password : newpassword}});
+            res.render("courses/setting",{text:"Password Update successfully"})
+            }
+            else{
+            res.render("courses/setting",{text:"Wrong Email or password"})
+            }
+        }
+        else{
+            res.render("courses/setting",{text: "password and confirm password not matched"})
+        }
+    } catch (error) {
+        res.render("courses/setting",{text:"Wrong Email or password"})
+    }
 })
 
 app.post("/delete_account", async (req, res) => {
     const deleteemail = await User.findOne({ email: req.body.email })
     if (deleteemail.password === req.body.password) {
         const deleteuser = await User.deleteOne({ email: req.body.email })
-        res.status(201).send("Your Account has been deleted successfully!!")
+        res.redirect("/");
     }
     else
         res.redirect("/setting")
